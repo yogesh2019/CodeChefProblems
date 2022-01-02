3259984ef96bd30fcfbe8642cfaeb60bf0511d5d
yogesh2019 <yogeshkkhichi@gmail.com>
2022-01-02 18:10:19 +0530

    BigInteger.valueOf, BigInteger.ZERO, A.multiply(B)

---
+++ beginner/_2_small_factorials.java
@@ -0,0 +1,22 @@
+package beginner;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.BigInteger;
+public class _2_small_factorials {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		while(n-- > 0){
+		    int num = in.nextInt();
+		    BigInteger mul = BigInteger.valueOf(num);
+		    while(--num > 0){
+		        mul = mul.multiply(BigInteger.valueOf(num));
+		    }
+		    System.out.println(mul);
+		}
+		
+	}
+}
